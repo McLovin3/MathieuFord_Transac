e4b028cf69b776e653d9121cfeb140d2a9b992db
McLovin3 <mathieu.ford@gmail.com>
2022-02-25 10:46:31 -0500

    Added DVD class and it's attributes

---
+++ src/main/java/model/document/DVD.java
@@ -0,0 +1,13 @@
+package model.document;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class DVD extends LibraryDocument
+{
+    private int runtime;
+}
