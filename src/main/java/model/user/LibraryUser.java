59f2adc938baf76da54a8176ce8a380517a2886a
McLovin3 <mathieu.ford@gmail.com>
2022-02-25 10:33:05 -0500

    Added LibraryUser class

---
+++ src/main/java/model/user/LibraryUser.java
@@ -0,0 +1,13 @@
+package model.user;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+public abstract class LibraryUser
+{
+
+}
