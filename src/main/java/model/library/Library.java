f8fcddfa22c5efb253835017c73222b1b1aeb891
McLovin3 <mathieu.ford@gmail.com>
2022-03-03 18:56:46 -0500

    Added Library class and it's attributes

---
+++ src/main/java/model/library/Library.java
@@ -0,0 +1,16 @@
+package model.library;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import model.document.LibraryDocument;
+import model.user.LibraryUser;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class Library
+{
+    private List<LibraryDocument> documents;
+    private List<LibraryUser> users;
+}
