21322817ca05fbba041677d1e01a26ba59557669
McLovin3 <mathieu.ford@gmail.com>
2022-03-04 17:47:14 -0500

    createClient function throws exception if pk already exists

--- src/main/java/service/AttendantService.java
+++ src/main/java/service/AttendantService.java
@@ -3,16 +3,22 @@ package service;
 import lombok.Data;
 import model.user.Client;
 import model.user.LibraryUser;
-import persistence.AttendantDAO;
+import persistence.AttendantDao;
+
+import javax.persistence.EntityExistsException;
 
 @Data
 public class AttendantService
 {
-    private AttendantDAO clientDAO;
+    private AttendantDao clientDAO;
 
-    public void createClient(String username, String password)
+    public void createClient(String username, String password) throws IllegalArgumentException
     {
-        LibraryUser client = Client.builder().username(username).password(password).build();
-        clientDAO.addClient(client);
+        try
+        {
+            LibraryUser client = Client.builder().username(username).password(password).build();
+            clientDAO.addClient(client);
+        }
+        catch (EntityExistsException exception) {throw new IllegalArgumentException("Username already exists");}
     }
 }
