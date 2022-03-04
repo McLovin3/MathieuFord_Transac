518b20bddfe2ebd3c3b04ab76b5dd66039c6c586
McLovin3 <mathieu.ford@gmail.com>
2022-03-04 18:44:39 -0500

    Changed hibernate config project name

--- src/main/java/persistence/AttendantDaoJpa.java
+++ src/main/java/persistence/AttendantDaoJpa.java
@@ -9,7 +9,7 @@ import javax.persistence.Persistence;
 
 public class AttendantDaoJpa implements AttendantDao
 {
-    private final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TP1.exe");
+    private final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("transac.exe");
 
     @Override
     public void addClient(LibraryUser client) throws EntityExistsException
