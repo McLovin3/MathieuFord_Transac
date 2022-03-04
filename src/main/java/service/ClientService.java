698adbcbade516d31ee651f6ef763d4ad8ed5ab7
McLovin3 <mathieu.ford@gmail.com>
2022-03-03 20:58:01 -0500

    Created createClient function

--- src/main/java/service/ClientService.java
+++ src/main/java/service/ClientService.java
@@ -1,6 +1,18 @@
 package service;
 
+import lombok.Data;
+import model.user.Client;
+import model.user.LibraryUser;
+import persistence.ClientDAO;
+
+@Data
 public class ClientService
 {
+    private ClientDAO clientDAO;
 
+    public void createClient(String username, String password) throws IllegalArgumentException
+    {
+        LibraryUser client = Client.builder().username(username).password(password).build();
+        clientDAO.addClient(client);
+    }
 }
