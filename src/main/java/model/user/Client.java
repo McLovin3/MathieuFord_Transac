c6cf7d346f797f6bce92623b33ff2f9749364b4c
McLovin3 <mathieu.ford@gmail.com>
2022-03-03 19:14:04 -0500

    Added Fine list to Client

--- src/main/java/model/user/Client.java
+++ src/main/java/model/user/Client.java
@@ -3,10 +3,14 @@ package model.user;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import model.library.Fine;
+
+import java.util.List;
 
 @Data
 @EqualsAndHashCode (callSuper = true)
 @NoArgsConstructor
 public class Client extends LibraryUser
 {
+    private List<Fine> Fines;
 }
