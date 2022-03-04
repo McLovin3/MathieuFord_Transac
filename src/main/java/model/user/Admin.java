806506d13f16a371d52dbef977c5a18d81997a8c
McLovin3 <mathieu.ford@gmail.com>
2022-03-03 19:14:25 -0500

    Create Admin class

---
+++ src/main/java/model/user/Admin.java
@@ -0,0 +1,12 @@
+package model.user;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class Admin extends LibraryUser
+{
+}
