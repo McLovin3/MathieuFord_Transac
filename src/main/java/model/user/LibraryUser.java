1db8916056faee472f52e16106df32be02cde6cf
McLovin3 <mathieu.ford@gmail.com>
2022-03-03 20:40:33 -0500

    Modified Annotations

--- src/main/java/model/user/LibraryUser.java
+++ src/main/java/model/user/LibraryUser.java
@@ -1,12 +1,23 @@
 package model.user;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class LibraryUser
 {
+    @Id
     private String username;
     private String password;
 }
