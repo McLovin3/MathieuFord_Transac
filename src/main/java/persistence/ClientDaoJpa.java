1f80a1201afd24a87df8969ec384a555bf779614
McLovin3 <mathieu.ford@gmail.com>
2022-03-04 19:19:20 -0500

    Did the getBorrows function

--- src/main/java/persistence/ClientDaoJpa.java
+++ src/main/java/persistence/ClientDaoJpa.java
@@ -2,8 +2,10 @@ package persistence;
 
 import model.library.Borrow;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class ClientDaoJpa implements ClientDao
@@ -13,6 +15,15 @@ public class ClientDaoJpa implements ClientDao
     @Override
     public List<Borrow> getBorrows(String username)
     {
-        return null;
+        EntityManager em = this.entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        TypedQuery<Borrow> query = em.createQuery("SELECT borrow FROM Borrow borrow WHERE borrow.client.username LIKE :usernameToSearch", Borrow.class);
+        query.setParameter("usernameToSearch", "%" + username +"%");
+        List<Borrow> borrows = query.getResultList();
+
+        em.getTransaction().commit();
+        em.close();
+        return borrows;
     }
 }
