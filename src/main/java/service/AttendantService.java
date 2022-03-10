05b50c5b1fb64c0cf47e6dbfe48f6ba9fa91a7a4
McLovin3 <mathieu.ford@gmail.com>
2022-03-10 09:11:03 -0500

    Added createBook function

--- src/main/java/service/AttendantService.java
+++ src/main/java/service/AttendantService.java
@@ -1,6 +1,7 @@
 package service;
 
 import lombok.Data;
+import model.document.Book;
 import model.user.Client;
 import model.user.LibraryUser;
 import persistence.LibraryDao;
@@ -8,11 +9,25 @@ import persistence.LibraryDao;
 @Data
 public class AttendantService
 {
-    private LibraryDao clientDAO;
+    private final LibraryDao LIBRARY_DAO;
 
-    public void addClient(String username, String password)
+    public void createClient(String username, String password)
     {
         LibraryUser client = Client.builder().username(username).password(password).build();
-        clientDAO.addClient(client);
+        LIBRARY_DAO.saveClient(client);
+    }
+
+    public void createBook(String title, String author, int publicationYear, int nbCopies, String editor, int nbPages, String bookType) throws IllegalArgumentException
+    {
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .publicationYear(publicationYear)
+                .nbCopies(nbCopies)
+                .editor(editor)
+                .nbPages(nbPages)
+                .bookType(model.document.BookType.getBookType(bookType))
+                .build();
+        LIBRARY_DAO.saveBook(book);
     }
 }
