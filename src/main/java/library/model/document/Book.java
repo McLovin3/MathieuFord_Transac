eae61b402624a45dad99b573953ef76f8551f29b
McLovin3 <mathieu.ford@gmail.com>
2022-03-12 16:13:49 -0500

    Added ToString

--- src/main/java/library/model/document/Book.java
+++ src/main/java/library/model/document/Book.java
@@ -16,4 +16,20 @@ public class Book extends LibraryDocument
     private String editor;
     private int nbPages;
     private BookType bookType;
+
+    @Override
+    public String toString()
+    {
+        return "Book{" +
+                "editor='" + editor + '\'' +
+                ", nbPages=" + nbPages +
+                ", bookType=" + bookType +
+                ", id=" + id +
+                ", library=" + library.toString() +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publicationYear=" + publicationYear +
+                ", nbCopies=" + nbCopies +
+                '}';
+    }
 }
