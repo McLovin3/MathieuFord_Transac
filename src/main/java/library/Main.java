b8c02b235df1adad7d779b9ab29cd97c2dc26ff0
McLovin3 <mathieu.ford@gmail.com>
2022-03-12 15:19:08 -0500

    Added test code to main

--- src/main/java/library/Main.java
+++ src/main/java/library/Main.java
@@ -1,9 +1,27 @@
 package library;
 
+import library.persistence.LibraryDao;
+import library.persistence.LibraryDaoJpaH2;
+import library.service.AttendantService;
+import library.service.ClientService;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        LibraryDao libraryDao = new LibraryDaoJpaH2();
+        AttendantService attendantService = new AttendantService(libraryDao);
+        ClientService clientService = new ClientService(libraryDao);
 
+        attendantService.createLibrary("CAL");
+        attendantService.createClient("Mathieu", "password");
+        attendantService.addClientToLibrary(0, 0);
+        attendantService.createBook("20 thousand leagues under the sea", "Jules Vernes", 1870, 2, "Pierre-Jules Hetzel", 300, "novel");
+        attendantService.createBook("Clean Code", "Robert Martin", 2008, 1, "Robert C. martin", 200, "study");
+        attendantService.addBookToLibrary(0, 0);
+        attendantService.addBookToLibrary(1, 0);
+
+        System.out.println(attendantService.getLibrary(0).getDOCUMENTS().get(0).toString());
+        System.out.println(attendantService.getLibrary(0).getDOCUMENTS().get(1).toString());
     }
 }
