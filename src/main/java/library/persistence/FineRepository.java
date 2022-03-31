ce80d391b0ec3fcf4510ca844e7ae4c0507db4e4
McLovin3 <mathieu.ford@gmail.com>
2022-03-31 14:06:49 -0400

    Created FineRepository

---
+++ src/main/java/library/persistence/FineRepository.java
@@ -0,0 +1,9 @@
+package library.persistence;
+
+import library.model.library.Fine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FineRepository extends JpaRepository<Fine, Long>
+{
+
+}
