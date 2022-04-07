63f4e27b71ef138d7be97b2929fdd0898b3769c4
McLovin3 <mathieu.ford@gmail.com>
2022-04-07 13:19:12 -0400

    Added findAllByClient

--- src/main/java/library/persistence/FineRepository.java
+++ src/main/java/library/persistence/FineRepository.java
@@ -1,8 +1,14 @@
 package library.persistence;
 
 import library.model.library.Fine;
+import library.model.user.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface FineRepository extends JpaRepository<Fine, Long> {}
+public interface FineRepository extends JpaRepository<Fine, Long>
+{
+    List<Fine> findAllByClient(Client client);
+}
