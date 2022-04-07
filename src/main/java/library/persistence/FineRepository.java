0af84e5a9f90aa96296c6e94fa6be960ad6e1acd
McLovin3 <mathieu.ford@gmail.com>
2022-04-07 16:31:44 -0400

    Changed findAllByClientId function

--- src/main/java/library/persistence/FineRepository.java
+++ src/main/java/library/persistence/FineRepository.java
@@ -1,7 +1,6 @@
 package library.persistence;
 
 import library.model.library.Fine;
-import library.model.user.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +9,5 @@ import java.util.List;
 @Repository
 public interface FineRepository extends JpaRepository<Fine, Long>
 {
-    List<Fine> findAllByClient(Client client);
+    List<Fine> findAllByClientId(long clientId);
 }
