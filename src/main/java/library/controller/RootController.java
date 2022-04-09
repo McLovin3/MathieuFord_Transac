26538c82e74a10c79d33488cb5017e991444b99f
McLovin3 <mathieu.ford@gmail.com>
2022-04-09 15:30:07 -0400

    Added createBook page functionnality

--- src/main/java/library/controller/RootController.java
+++ src/main/java/library/controller/RootController.java
@@ -1,9 +1,12 @@
 package library.controller;
 
+import library.exception.NotEnoughCopiesException;
+import library.form.BookForm;
 import library.form.ClientForm;
 import library.service.AttendantService;
 import library.service.ClientService;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,7 +33,7 @@ public class RootController
         return "books";
     }
 
-    //TODO must use DTO
+    // TODO must use DTO
 
     @GetMapping("/clients")
     public String getClientsRequest(Model model)
@@ -40,17 +43,43 @@ public class RootController
     }
 
     @GetMapping("/createClient")
-    public String getClientEdit(Model model)
+    public String getCreateClient(Model model)
     {
-        model.addAttribute("clientForm", new ClientForm(0, "", ""));
+        model.addAttribute("clientForm", new ClientForm());
         return "createClient";
     }
 
-
     @PostMapping("/createClient")
-    public String postClient(@ModelAttribute ClientForm clientform, Model model)
+    public String postClient(@ModelAttribute ClientForm clientform)
     {
-        ATTENDANT_SERVICE.createClient(clientform.name(), clientform.password());
+        ATTENDANT_SERVICE.createClient(clientform.getName(), clientform.getPassword());
+        return "redirect:/";
+    }
+
+    @GetMapping("/createBook")
+    public String getCreateBook(Model model)
+    {
+        model.addAttribute("bookForm", new BookForm());
+        return "createBook";
+    }
+
+    @PostMapping("/createBook")
+    private String postBook(@ModelAttribute BookForm bookForm)
+    {
+        try
+        {
+            ATTENDANT_SERVICE.createBook(bookForm.getTitle(),
+                    bookForm.getAuthor(),
+                    bookForm.getPublicationYear(),
+                    bookForm.getNbCopies(),
+                    bookForm.getEditor(),
+                    bookForm.getNbPages(),
+                    bookForm.getBookType());
+        }
+        catch (NotEnoughCopiesException exception)
+        {
+        }
+        // TODO how to manage exception?
         return "redirect:/";
     }
 }
