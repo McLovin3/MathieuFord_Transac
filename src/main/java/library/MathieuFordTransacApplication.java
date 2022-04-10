18cca1cd5f8567f288648d0515c69c12a346f819
McLovin3 <mathieu.ford@gmail.com>
2022-04-10 15:58:55 -0400

    Added test code

--- src/main/java/library/MathieuFordTransacApplication.java
+++ src/main/java/library/MathieuFordTransacApplication.java
@@ -1,6 +1,9 @@
 package library;
 
+import library.dto.BookDTO;
+import library.dto.ClientDTO;
 import library.service.AttendantService;
+import library.service.ClientService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MathieuFordTransacApplication implements CommandLineRunner
 {
     private final AttendantService ATTENDANT_SERVICE;
+    private final ClientService CLIENT_SERVICE;
 
     public static void main(String[] args)
     {
@@ -21,9 +25,9 @@ public class MathieuFordTransacApplication implements CommandLineRunner
     @Override
     public void run(String... args) throws Exception
     {
-        ATTENDANT_SERVICE.createClient("Mathieu", "password");
-        ATTENDANT_SERVICE.createBook("20 thousand leagues under the sea", "Jules Vernes", 1870, 2,
-                "Pierre-Jules Hetzel", 300, "NOVEL");
-        ATTENDANT_SERVICE.createBook("Clean Code", "Robert Martin", 2008, 1, "Robert C. martin", 200, "study");
+        ATTENDANT_SERVICE.createClient(new ClientDTO(0, "Mathieu", "password"));
+        ATTENDANT_SERVICE.createBook(new BookDTO(0, "20 thousand leagues under the sea", "Jules Vernes", "NOVEL", "Pierre-Jules Hetzel", 300, 1870, 2));
+        ATTENDANT_SERVICE.createBook(new BookDTO(0, "Clean Code", "Robert Martin", "study", "Robert C. Martin", 200, 2008,2));
+        System.out.println(CLIENT_SERVICE.searchBooksByYear(1870).get(0).toString());
     }
 }
