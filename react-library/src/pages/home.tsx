3d67c6f7c749eb22e7e2b30ed033af2dca7c4363
McLovin3 <mathieu.ford@gmail.com>
2022-04-24 16:02:53 -0400

    Put home page in it's own file

---
+++ react-library/src/pages/home.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import ButtonComponent from "../components/ButtonComponent";
+import UsersComponent from "../components/UsersComponent";
+
+function Home() {
+    const clients = [
+        {
+            id: 1,
+            name: 'Homer',
+        },
+        {
+            id: 2,
+            name: 'Bart',
+        },
+        {
+            id: 3,
+            name: 'Maggie',
+        },
+    ];
+
+    const employees = [
+        {
+            id: 1,
+            name: 'Fry',
+        },
+        {
+            id: 2,
+            name: 'Leela',
+        },
+        {
+            id: 3,
+            name: 'Bender',
+        },
+    ];
+
+    const [users, setUsers] = useState(clients);
+    const [isClients, setIsClients] = useState(true);
+
+    return (<div style={{ textAlign: "center" }}>
+        <h1>Bibliothèque Java Town</h1>
+        <h2>{isClients ? "Clients : " : "Employés : "}</h2>
+        <UsersComponent users={users}></UsersComponent>
+        <ButtonComponent text={isClients ? "Je suis un employé" : "Je suis un client"} onClick={() => {
+            setIsClients(!isClients);
+            setUsers(!isClients ? clients : employees);
+        }}></ButtonComponent>
+    </div >);
+}
+
+export default Home;
