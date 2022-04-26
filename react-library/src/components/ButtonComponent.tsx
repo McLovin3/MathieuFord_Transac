3c779390769638d5c69f0c2510f20634229e9f87
McLovin3 <mathieu.ford@gmail.com>
2022-04-26 10:17:56 -0400

    Added disabled prop

--- react-library/src/components/ButtonComponent.tsx
+++ react-library/src/components/ButtonComponent.tsx
@@ -1,3 +1,4 @@
-export default function ButtonComponent({ text = "Button", onClick = () => { }, color = "blue" }: { text: string; onClick: any; color?: string }) {
-    return (<button onClick={onClick} style={{ backgroundColor: color }}>{text}</button>)
+export default function ButtonComponent({ text = "Button", onClick = () => { }, color = "pink", disabled }:
+    { text: string; onClick: any; color?: string, disabled: boolean }) {
+    return (<button onClick={onClick} disabled={disabled} style={{ backgroundColor: color }}>{text}</button>)
 }
