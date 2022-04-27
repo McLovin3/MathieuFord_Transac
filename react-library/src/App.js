5489fe184dc5791c5c2d54e2d1cffd8710666f82
McLovin3 <mathieu.ford@gmail.com>
2022-04-27 11:21:49 -0400

    Added useEffect to get employees from server

--- react-library/src/App.js
+++ react-library/src/App.js
@@ -1,53 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import ButtonComponent from './components/ButtonComponent';
 import UsersComponent from './components/UsersComponents';
 
 function App() {
-  const clients = [
-    {
-      id: 1,
-      name: 'Homer',
-    },
-    {
-      id: 2,
-      name: 'Bart',
-    },
-    {
-      id: 3,
-      name: 'Maggie',
-    },
-  ];
 
-  const employees = [
-    {
-      id: 1,
-      name: 'Fry',
-    },
-    {
-      id: 2,
-      name: 'Leela',
-    },
-    {
-      id: 3,
-      name: 'Bender',
-    },
-  ];
+  useEffect(() => {
+    const getEmployees = async () => {
+      const employees = await fetchEmployees();
+      setUsers(employees);
+    }
+    getEmployees();
+  }, []);
 
-  const [users, setUsers] = useState(clients);
-  const [isClients, setIsClients] = useState(true);
-
-  const onClick = () => {
-    setIsClients(!isClients);
-    isClients ? setUsers(employees) : setUsers(clients);
+  const fetchEmployees = async () => {
+    const response = await fetch("http://localhost:5000/employees");
+    const employees = await response.json();
+    return employees;
   }
+
+  const [users, setUsers] = useState([]);
 
   return (
     <div className="container">
       <h1>Bibliotèque Java town</h1>
       <UsersComponent users={users}></UsersComponent>
-      <ButtonComponent color={isClients ? "pink" : "red"} text="Clients" disabled={isClients} onClick={onClick}></ButtonComponent>
-      <ButtonComponent color={isClients ? "red" : "pink"} text="Employé" disabled={!isClients} onClick={onClick}></ButtonComponent>
     </div >
   );
 }
