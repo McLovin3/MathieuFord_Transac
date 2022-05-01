d42f9ecb209d9b0de5067b1641ead459e3170c8e
McLovin3 <mathieu.ford@gmail.com>
2022-05-01 15:51:28 -0400

    Added fetch for url id

--- react-library/src/components/AttendantComponent.js
+++ react-library/src/components/AttendantComponent.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const AttendantComponent = () => {
+
+    const [attendant, setAttendant] = useState("");
+
+    useEffect(() => {
+        const getAttendant = async () => {
+            const attendant = await fetchAttendant();
+            setAttendant(attendant);
+        }
+        getAttendant();
+    }, []);
+
+
+    const fetchAttendant = async () => {
+        const response = await fetch("http://localhost:5000/attendants/" + window.location.href.split("/").pop());
+        const attendant = await response.json();
+        return attendant;
+    }
+
     return (
         <div className="row p-5">
+            <h2>{attendant.name}</h2>
             <div className="col-6">
                 <Link className="btn btn-primary" to="addClient">Ajouter un client</Link>
             </div>
