d99c8eaa87c1b22ea37096ea8f7cc353f19987c3
McLovin3 <mathieu.ford@gmail.com>
2022-05-01 15:51:37 -0400

    Added fetch for url id

--- react-library/src/components/ClientComponent.js
+++ react-library/src/components/ClientComponent.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const ClientComponent = () => {
+
+    const [client, setClient] = useState("");
+
+    useEffect(() => {
+        const getClient = async () => {
+            const client = await fetchClient();
+            setClient(client);
+        }
+        getClient();
+    }, []);
+
+    const fetchClient = async () => {
+        const response = await fetch("http://localhost:5000/clients/" + window.location.href.split("/").pop());
+        const client = await response.json();
+        return client;
+    }
+
     return (
         <div className="row p-5">
+            <h2>{client.name}</h2>
             <div className="col-6">
                 <Link className="btn btn-primary" to="books">Livres</Link>
             </div>
