9326da8edc1b199b8159d8afc9403da7c292207b
McLovin3 <mathieu.ford@gmail.com>
2022-05-02 10:08:20 -0400

    Add Xs when field is null

--- react-library/src/components/DocumentComponent.js
+++ react-library/src/components/DocumentComponent.js
@@ -5,11 +5,11 @@ const DocumentComponent = ({ document }) => {
         <tr>
             <td>{document.title}</td>
             <td>{document.author}</td>
-            <td>{document.bookType}</td>
-            <td>{document.nbPages}</td>
-            <td>{document.editor}</td>
+            <td>{document.bookType == null ? "X" : document.bookType}</td>
+            <td>{document.nbPages == null ? "X" : document.nbPages}</td>
+            <td>{document.editor == null ? "X" : document.editor}</td>
             <td>{document.publicationYear}</td>
-            <td>{document.runtime}</td>
+            <td>{document.runtime == null ? "X" : document.runtime}</td>
             <td>{document.documentType}</td>
             <td>{document.nbCopies}</td>
         </tr>
