b1795515430bd02a6423e9b02eeb5905d74c2177
McLovin3 <mathieu.ford@gmail.com>
2022-05-02 10:00:14 -0400

    Added form

--- react-library/src/components/AddCDComponent.js
+++ react-library/src/components/AddCDComponent.js
@@ -1,7 +1,66 @@
 import React from "react";
+import { useState } from "react";
 
 const AddCDComponent = () => {
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [publicationYear, setPublicationYear] = useState("");
+    const [runtime, setRuntime] = useState("");
+    const [nbCopies, setNbCopies] = useState("");
 
+    const postCD = async (CD) => {
+        await fetch("http://localhost:5000/documents",
+            {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(CD)
+            });
+    }
+
+    const onSubmit = async (form) => {
+        form.preventDefault();
+        await postCD({
+            title,
+            author,
+            publicationYear,
+            runtime,
+            nbCopies,
+            documentType: "CD"
+        });
+        setTitle("");
+        setAuthor("");
+        setPublicationYear("");
+        setRuntime("");
+        setNbCopies("");
+    }
+
+    return (
+        <form className="col-10 mx-auto" onSubmit={onSubmit}>
+            <div className="form-group">
+                <label>Titre</label>
+                <input className="form-control" required minLength="3" type="text" value={title} onChange={(field) => setTitle(field.target.value)}></input>
+            </div>
+            <div className="form-group">
+                <label>Autheur</label>
+                <input className="form-control" required minLength="3" type="text" value={author} onChange={(field) => setAuthor(field.target.value)}></input>
+            </div>
+            <div className="form-group">
+                <label>Année de publication</label>
+                <input className="form-control" required min="1" type="number" value={publicationYear} onChange={(field) => setPublicationYear(field.target.value)}></input>
+            </div>
+            <div className="form-group">
+                <label>Durée</label>
+                <input className="form-control" required min="1" type="number" value={runtime} onChange={(field) => setRuntime(field.target.value)}></input>
+            </div>
+            <div className="form-group">
+                <label>Nombre d'exemplaires</label>
+                <input className="form-control" required min="1" type="number" value={nbCopies} onChange={(field) => setNbCopies(field.target.value)}></input>
+            </div>
+            <input type="submit" value="Enregistrer" className="btn btn-primary" />
+        </form>
+    );
 }
 
 export default AddCDComponent;
