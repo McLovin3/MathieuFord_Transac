742f937e40d3c4d573de37164a2ac89b0570a8b1
McLovin3 <mathieu.ford@gmail.com>
2022-05-03 09:28:09 -0400

    Changed discriminator value

--- src/main/java/library/model/user/Attendant.java
+++ src/main/java/library/model/user/Attendant.java
@@ -12,7 +12,7 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @NoArgsConstructor
 @Setter
-@DiscriminatorValue("Attendant")
+@DiscriminatorValue("ATTENDANT")
 @Entity
 public class Attendant extends LibraryUser
 {
