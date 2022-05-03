0c759134cc6c4fda1f00e365545ad41f0e1e7f5f
McLovin3 <mathieu.ford@gmail.com>
2022-05-03 09:15:17 -0400

    Adding attendant

--- src/main/java/library/MathieuFordTransacApplication.java
+++ src/main/java/library/MathieuFordTransacApplication.java
@@ -26,8 +26,11 @@ public class MathieuFordTransacApplication implements CommandLineRunner
     public void run(String... args) throws Exception
     {
         attendantService.createClient(new UserDTO(0, "Mathieu", "password"));
-        attendantService.createBook(new DocumentDTO(0, "20 thousand leagues under the sea", "Jules Vernes", "NOVEL", "Pierre-Jules Hetzel", 300, 1870, 2, "BOOK", 0));
-        attendantService.createBook(new DocumentDTO(0, "Clean Code", "Robert Martin", "study", "Robert C. Martin", 200, 2008,2, "BOOK", 0));
+        attendantService.createAttendant(new UserDTO(0, "Admin", "admin"));
+        attendantService.createBook(new DocumentDTO(0, "20 thousand leagues under the sea", "Jules Vernes", "NOVEL",
+                "Pierre-Jules Hetzel", 300, 1870, 2, "BOOK", 0));
+        attendantService.createBook(new DocumentDTO(0, "Clean Code", "Robert Martin", "study", "Robert C. Martin", 200,
+                2008, 2, "BOOK", 0));
         System.out.println(clientService.searchDocumentsByYear(1870).get(0).toString());
         System.out.println(clientService.getAllDocuments().get(0).toString());
         System.out.println(clientService.getAllBooks().get(0).toString());
