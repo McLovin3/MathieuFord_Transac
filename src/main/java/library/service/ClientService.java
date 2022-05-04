71f064f2b7b8abe96f813dac672fc4302afed1d6
McLovin3 <mathieu.ford@gmail.com>
2022-05-04 12:01:09 -0400

    Reformated

--- src/main/java/library/service/ClientService.java
+++ src/main/java/library/service/ClientService.java
@@ -26,40 +26,48 @@ import static java.time.temporal.ChronoUnit.DAYS;
 
 @Service
 @RequiredArgsConstructor
-public class ClientService {
+public class ClientService
+{
     private final LibraryDocumentRepository documentRepo;
     private final ClientRepository clientRepo;
     private final BorrowRepository borrowRepo;
     private final FineRepository fineRepo;
 
-    public List<BookDTO> getAllBooks() {
+    public List<BookDTO> getAllBooks()
+    {
         return DataConversion.booksToDTO(documentRepo.findAllBooks());
     }
 
-    public List<DocumentDTO> getAllDocuments() {
+    public List<DocumentDTO> getAllDocuments()
+    {
         return DataConversion.documentsToDTO(documentRepo.findAll());
     }
 
-    public List<DocumentDTO> searchDocumentsByTitle(String title) {
+    public List<DocumentDTO> searchDocumentsByTitle(String title)
+    {
         return DataConversion.documentsToDTO(documentRepo.findAllByTitleIgnoreCaseContaining(title));
     }
 
-    public List<DocumentDTO> searchDocumentsByAuthor(String author) {
+    public List<DocumentDTO> searchDocumentsByAuthor(String author)
+    {
         return DataConversion.documentsToDTO(documentRepo.findAllByAuthorIgnoreCaseContaining(author));
     }
 
-    public List<DocumentDTO> searchDocumentsByYear(int year) {
+    public List<DocumentDTO> searchDocumentsByYear(int year)
+    {
         return DataConversion.documentsToDTO(documentRepo.findAllByPublicationYear(year));
     }
 
-    public List<BookDTO> searchBooksByCategory(String category) {
+    public List<BookDTO> searchBooksByCategory(String category)
+    {
         return DataConversion.booksToDTO(documentRepo.findAllBooksByCategory(BookType.getBookType(category)));
     }
 
     @Transactional
     public void borrowReturnDocument(long clientId, long documentId)
             throws ClientDidNotBorrowException, NonExistentUserException, NonExistentDocumentException,
-            DocumentAlreadyReturnException {
+            DocumentAlreadyReturnException
+    {
         Client client = getClientWithFinesAndBorrows(clientId);
         LibraryDocument document = getDocument(documentId);
 
@@ -76,14 +84,16 @@ public class ClientService {
         clientRepo.save(client);
     }
 
-    private LibraryDocument getDocument(long documentId) throws NonExistentDocumentException {
+    private LibraryDocument getDocument(long documentId) throws NonExistentDocumentException
+    {
         Optional<LibraryDocument> documentOptional = documentRepo.findById(documentId);
         if (documentOptional.isEmpty())
             throw new NonExistentDocumentException();
         return documentOptional.get();
     }
 
-    private Client getClientWithFinesAndBorrows(long clientId) throws NonExistentUserException {
+    private Client getClientWithFinesAndBorrows(long clientId) throws NonExistentUserException
+    {
         Optional<Client> clientOptional = clientRepo.findByIdWithFines(clientId);
 
         if (clientOptional.isEmpty())
@@ -94,10 +104,12 @@ public class ClientService {
         return client;
     }
 
-    private void calculateFines(Client client, Borrow borrow) {
+    private void calculateFines(Client client, Borrow borrow)
+    {
         LocalDateTime currentDate = LocalDateTime.now();
         LocalDateTime documentReturnDate = borrow.getReturnDate();
-        if (currentDate.isAfter(documentReturnDate)) {
+        if (currentDate.isAfter(documentReturnDate))
+        {
             Fine fine = Fine.builder()
                     .client(client)
                     .amount(DAYS.between(documentReturnDate, currentDate) * 0.25)
@@ -111,7 +123,8 @@ public class ClientService {
     @Transactional
     public void borrowDocument(long clientId, long documentId) throws NonExistentDocumentException,
             ClientHasFinesException, NoMoreCopiesException, NonExistentUserException,
-            ClientAlreadyHasBorrowException {
+            ClientAlreadyHasBorrowException
+    {
         Client client = getClientWithFinesAndBorrows(clientId);
         LibraryDocument document = getDocument(documentId);
         manageBorrowDocumentExceptions(document, client);
@@ -135,26 +148,32 @@ public class ClientService {
 
     private void manageBorrowDocumentExceptions(LibraryDocument document, Client client)
             throws ClientHasFinesException,
-            NoMoreCopiesException, ClientAlreadyHasBorrowException {
+            NoMoreCopiesException, ClientAlreadyHasBorrowException
+    {
         if (client.hasFines())
             throw new ClientHasFinesException();
 
         if (document.getNbCopies() == 0)
             throw new NoMoreCopiesException();
 
-        try {
+        try
+        {
             Borrow borrow = client.getBorrow(document.getId());
             if (!borrow.isReturned())
                 throw new ClientAlreadyHasBorrowException();
-        } catch (ClientDidNotBorrowException exception) {
+        }
+        catch (ClientDidNotBorrowException exception)
+        {
         }
     }
 
-    public List<BorrowDTO> getClientBorrows(long clientId) {
+    public List<BorrowDTO> getClientBorrows(long clientId)
+    {
         return DataConversion.borrowsToDTO(borrowRepo.findAllByClientId(clientId));
     }
 
-    public List<FineDTO> getClientFines(long clientId) {
+    public List<FineDTO> getClientFines(long clientId)
+    {
         return DataConversion.finesToDTO(fineRepo.findAllByClientId(clientId));
     }
 }
