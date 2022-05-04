6ddac57d375e2cf8236a363b3725b5e12a206228
McLovin3 <mathieu.ford@gmail.com>
2022-05-04 12:29:56 -0400

    Added validation

--- src/main/java/library/dto/DocumentDTO.java
+++ src/main/java/library/dto/DocumentDTO.java
@@ -1,5 +1,8 @@
 package library.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,13 +15,19 @@ import lombok.NoArgsConstructor;
 public class DocumentDTO
 {
     private long id;
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
     private String bookType;
     private String editor;
     private int nbPages;
+    @Min(1)
     private int publicationYear;
+    @Min(1)
     private int nbCopies;
+    @NotBlank
     private String documentType;
+    @Min(1)
     private int runtime;
 }
--- src/main/java/library/dto/UserDTO.java
+++ src/main/java/library/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package library.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +14,8 @@ import lombok.NoArgsConstructor;
 public class UserDTO
 {
     private long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String password;
 }
